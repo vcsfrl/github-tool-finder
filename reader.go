2344586217e4ba9f49fc6f3dae788eb78a14e6f9
Varga Csaba <vcsfrl@gmail.com>
2020-05-11 17:46:11 +0300

    Rearrange calls.

--- reader.go
+++ reader.go
@@ -13,6 +13,8 @@ type HTTPClient interface {
 	Do(r *http.Request) (*http.Response, error)
 }
 
+const repoSearchQuery = "{\"query\":\"query SearchRepositories {\\n  search(query: \\\"%s\\\", type: REPOSITORY, first:%d%s){\\n    repositoryCount\\n    edges {\\n      cursor \\n      node {\\n\\t\\t\\t\\t... on Repository {\\n          description\\n          name\\n          nameWithOwner\\n          url\\n          owner {\\n            login\\n          }\\n          forkCount\\n          stargazers {\\n            totalCount\\n          }\\n          watchers {\\n            totalCount\\n          }\\n          homepageUrl\\n          licenseInfo {\\n            name\\n          }\\n          mentionableUsers {\\n            totalCount\\n          }\\n          mirrorUrl\\n          isMirror\\n          primaryLanguage {\\n            name\\n          }\\n          parent {\\n            name\\n          }\\n          createdAt\\n          updatedAt\\n        }\\n      }\\n    }\\n  }\\n}\\n\",\"variables\":{}}"
+
 type SearchReader struct {
 	query    string
 	total    int
@@ -34,17 +36,30 @@ func (this *SearchReader) Handle() error {
 	return this.paginatedRead()
 }
 
+func (this *SearchReader) adjustPageSize() {
+	if this.pageSize > this.total {
+		this.pageSize = this.total
+	}
+}
+
 func (this *SearchReader) paginatedRead() error {
 	var result *SearchResponse
 	var cursor string
 	for i := 0; i < this.total; i = i + this.pageSize {
 		result = this.readRepositories(this.calculateLimit(i), this.findCursor(result, cursor))
-		err := this.sendResult(result)
-		if nil != err {
+		if err := this.sendResult(result); nil != err {
 			return err
 		}
 	}
 	return nil
+}
+
+func (this *SearchReader) calculateLimit(readIndex int) int {
+	limit := this.pageSize
+	if readIndex+this.pageSize > this.total {
+		limit = this.total - readIndex
+	}
+	return limit
 }
 
 func (this *SearchReader) findCursor(previousResult *SearchResponse, cursor string) string {
@@ -54,18 +69,15 @@ func (this *SearchReader) findCursor(previousResult *SearchResponse, cursor stri
 	return cursor
 }
 
-func (this *SearchReader) adjustPageSize() {
-	if this.pageSize > this.total {
-		this.pageSize = this.total
+func (this *SearchReader) readRepositories(limit int, cursor string) *SearchResponse {
+	result := &SearchResponse{}
+	reader, err := this.repositoryQueryReader(this.buildQl(limit, cursor))
+	if nil != reader {
+		defer reader.Close()
 	}
-}
+	this.decodeRepositories(reader, result, err)
 
-func (this *SearchReader) calculateLimit(readIndex int) int {
-	limit := this.pageSize
-	if readIndex+this.pageSize > this.total {
-		limit = this.total - readIndex
-	}
-	return limit
+	return result
 }
 
 func (this *SearchReader) sendResult(result *SearchResponse) error {
@@ -79,6 +91,35 @@ func (this *SearchReader) sendResult(result *SearchResponse) error {
 	}
 
 	return nil
+}
+
+func (this *SearchReader) decodeRepositories(reader io.ReadCloser, result *SearchResponse, err error) {
+	if nil != err {
+		result.Message = err.Error()
+		return
+	}
+	decoder := json.NewDecoder(reader)
+	err = decoder.Decode(result)
+	if nil != err {
+		result.Message = err.Error()
+	}
+}
+
+func (this *SearchReader) repositoryQueryReader(query string) (io.ReadCloser, error) {
+	request, _ := http.NewRequest("POST", "", strings.NewReader(query))
+	response, err := this.client.Do(request)
+	if nil != err {
+		return nil, err
+	}
+
+	return response.Body, nil
+}
+
+func (this *SearchReader) buildQl(limit int, cursor string) string {
+	if "" != cursor {
+		cursor = fmt.Sprintf(", after: \\\"%s\\\"", cursor)
+	}
+	return fmt.Sprintf(repoSearchQuery, this.query, limit, cursor)
 }
 
 func (this *SearchReader) getErrors(result *SearchResponse) (error, bool) {
@@ -101,48 +142,6 @@ func (this *SearchReader) wrapErrors(result *SearchResponse) error {
 	return err
 }
 
-func (this *SearchReader) readRepositories(limit int, cursor string) *SearchResponse {
-	result := &SearchResponse{}
-	reader, err := this.repositoryReader(limit, cursor)
-	if nil != reader {
-		defer reader.Close()
-	}
-	this.decodeRepositories(reader, result, err)
-
-	return result
-}
-
-func (this *SearchReader) decodeRepositories(reader io.ReadCloser, result *SearchResponse, err error) {
-	if nil != err {
-		result.Message = err.Error()
-		return
-	}
-	decoder := json.NewDecoder(reader)
-	err = decoder.Decode(result)
-	if nil != err {
-		result.Message = err.Error()
-	}
-}
-
-func (this *SearchReader) repositoryReader(limit int, cursor string) (io.ReadCloser, error) {
-	request, _ := http.NewRequest("POST", "", strings.NewReader(this.buildQl(limit, cursor)))
-	response, err := this.client.Do(request)
-	if nil != err {
-		return nil, err
-	}
-
-	return response.Body, nil
-}
-
-func (this *SearchReader) buildQl(limit int, cursor string) string {
-	if "" != cursor {
-		cursor = fmt.Sprintf(", after: \\\"%s\\\"", cursor)
-	}
-	return fmt.Sprintf(repoSearchQuery, this.query, limit, cursor)
-}
-
 func NewSearchReader(query string, total int, output chan *Repository, client HTTPClient) *SearchReader {
 	return &SearchReader{query: query, total: total, output: output, client: client, pageSize: 100}
 }
-
-const repoSearchQuery = "{\"query\":\"query SearchRepositories {\\n  search(query: \\\"%s\\\", type: REPOSITORY, first:%d%s){\\n    repositoryCount\\n    edges {\\n      cursor \\n      node {\\n\\t\\t\\t\\t... on Repository {\\n          description\\n          name\\n          nameWithOwner\\n          url\\n          owner {\\n            login\\n          }\\n          forkCount\\n          stargazers {\\n            totalCount\\n          }\\n          watchers {\\n            totalCount\\n          }\\n          homepageUrl\\n          licenseInfo {\\n            name\\n          }\\n          mentionableUsers {\\n            totalCount\\n          }\\n          mirrorUrl\\n          isMirror\\n          primaryLanguage {\\n            name\\n          }\\n          parent {\\n            name\\n          }\\n          createdAt\\n          updatedAt\\n        }\\n      }\\n    }\\n  }\\n}\\n\",\"variables\":{}}"
